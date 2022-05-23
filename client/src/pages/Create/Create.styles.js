2d58e5d729ba23220e2dbde273386833b415e823
wingram1 <waingram96@gmail.com>
2022-05-22 20:18:36 -0500

    create page styled

---
+++ client/src/pages/Create/Create.styles.js
@@ -0,0 +1,28 @@
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  createFormContainer: {
+    width: "100%",
+    maxWidth: "900px",
+    height: "fit-content",
+    backgroundColor: theme.colors.gray[0],
+    border: `1px ${theme.colors.gray[5]} solid`,
+    borderRadius: 8,
+    margin: "10px 0 10px 0",
+    padding: "5px 10px 10px 10px",
+  },
+  title: {
+    width: "75%",
+  },
+  categories: {
+    width: "75%",
+  },
+  description: {},
+  submit: {
+    display: "flex",
+    flexDirection: "row-reverse",
+    paddingTop: "20px",
+  },
+}));
+
+export default useStyles;
