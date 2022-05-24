9159a888aa9b31a1055d91b86bb8c9b7ed29b552
wingram1 <waingram96@gmail.com>
2022-05-23 21:44:47 -0500

    calendar date select & date highlights

--- client/src/pages/Home/subcomponents/UserLog.js
+++ client/src/pages/Home/subcomponents/UserLog.js
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
-import { getToday } from "../../utils/dateTools";
+import { getToday } from "../../../utils/dateTools";
+
+import useStyles from "../Home.styles";
 
 // import Calendar from "./subcomponents/Calendar";
 
 const today = getToday();
 
-function UserLog() {
-  // grab sessions from localStorage
-  const [userSessions, setUserSessions] = useState(
-    JSON.parse(localStorage.getItem("mySessions"))
-  );
+function UserLog(props) {
+  const { classes } = useStyles();
+
+  const { pickedDate, userSessions } = props;
 
   const mostRecent =
     Object.keys(userSessions)[Object.keys(userSessions).length - 1];
 
-  console.log(mostRecent);
+  console.log(pickedDate.toString());
 
   // map today's sessions, otherwise most recent (mySessions[mySessions.length - 1])
 
   return (
-    <div>
+    <div className={classes.logWrapper}>
       <h2>Activity Log</h2>
       {/* check for user sessions */}
       {userSessions ? (
@@ -28,7 +29,7 @@ function UserLog() {
             const session = userSessions[today][day];
             return (
               <div>
-                <h3>Today's Activity:</h3>
+                <h3>Activity for {pickedDate.toDateString()}</h3>
                 <p>
                   {session.title} - {session.time}
                   {/* TODO: map exercises and whatnot */}
@@ -42,7 +43,7 @@ function UserLog() {
             const session = userSessions[mostRecent][day];
             return (
               <div>
-                <h3>Today's Activity:</h3>
+                <h3>Most recent activity: </h3>
                 <p>
                   {session.title} - {session.time}
                   {/* TODO: map exercises and whatnot */}
